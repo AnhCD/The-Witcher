93f599049910038b262a1556b226e2a603d255b8
AnhCD <ohodavi1@gmail.com>
2021-07-25 11:49:28 +0700

    add asset menu for Weapon

--- Assets/Scripts/ScriptableObjects/Weapons/SO_WeaponData.cs
+++ Assets/Scripts/ScriptableObjects/Weapons/SO_WeaponData.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SO_WeaponData : MonoBehaviour
+[CreateAssetMenu(fileName = "newWeaponData", menuName = "Data/Weapon Data/Weapon")]
+public class SO_WeaponData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float[] movementSpeed;
 }
